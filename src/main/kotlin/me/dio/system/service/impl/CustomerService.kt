531b6013a0616f38b5716455db5b8139b7b7adff
yanpe <yan.pedro@outlook.com>
2023-10-29 00:31:39 -0300

    Creation of class CreditService, CostumerService for implements the repositories credit and customer

---
+++ src/main/kotlin/me/dio/system/service/impl/CustomerService.kt
@@ -0,0 +1,21 @@
+package me.dio.system.service.impl
+
+import me.dio.system.model.Customer
+import me.dio.system.repository.CustomerRepository
+import me.dio.system.service.ICustomerService
+import org.springframework.stereotype.Service
+
+@Service
+class CustomerService(
+    private val customerRepository: CustomerRepository
+) : ICustomerService {
+    override fun save(customer: Customer): Customer =
+        this.customerRepository.save(customer)
+
+    override fun findById(id: Long): Customer =
+        this.customerRepository.findById(id).orElseThrow {
+            throw RuntimeException("Id $id not found")
+        }
+
+    override fun delete(id: Long) = this.customerRepository.deleteById(id)
+}
