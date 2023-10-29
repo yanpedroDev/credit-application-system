b511217ddcd4841fdce28d563b74f9570d251203
yanpe <yan.pedro@outlook.com>
2023-10-28 22:49:20 -0300

    Interfaces Repositories JPA

---
+++ src/main/kotlin/me/dio/system/repository/CustomerRepository.kt
@@ -0,0 +1,9 @@
+package me.dio.system.repository
+
+import me.dio.system.model.Customer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomerRepository: JpaRepository<Customer, Long> {
+}
